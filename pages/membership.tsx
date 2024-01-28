ed4ec2c0789bd2c59cb5f3576c67d5d55906b69c
jbob9 <berlenskyoliver99@gmail.com>
2024-01-28 14:06:33 -0500

    begin to add dialog component and begin to create the about page

--- pages/membership.tsx
+++ pages/membership.tsx
@@ -1,16 +1,18 @@
 import { CheckmarkIcon, EnvelopeIcon } from '@sanity/icons'
-import React from 'react'
 
 const MemberShip = () => {
+
+  const handleOpen = () => {
+    window.open('https://www.zeffy.com/en-CA/embed/ticketing/cc3cccd9-ab4e-4eb9-ae05-d930d6a5bef7', 'membership', 'scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=100,top=100')
+  }
   return (
-    <div className="xl:container m-auto px-6 py-20 md:px-12 lg:px-20">
+    <div className="xl:container mx-3 md:m-auto py-24 md:px-12 lg:px-20">
       <div className="m-auto text-center lg:w-8/12 xl:w-7/12">
         <h2 className="text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">
           A Tailus Blocks subscription gives you access to our components and more.
         </h2>
       </div>
-      <div
-        className="m-auto mt-12 items-center justify-center -space-y-4 md:flex md:space-y-0 md:-space-x-4 xl:w-10/12"
+      <div className="m-auto mt-12 items-center justify-center -space-y-4 md:flex md:space-y-0 md:-space-x-4 xl:w-10/12"
       >
         <div className="group relative z-10 -mx-4 md:mx-0 md:w-6/12 lg:w-5/12">
           <div
@@ -18,7 +20,7 @@ const MemberShip = () => {
             className="absolute top-0 h-full w-full rounded-3xl border border-gray-100 dark:border-gray-700 dark:shadow-none bg-white dark:bg-gray-800 shadow-2xl shadow-gray-600/10 transition duration-500 group-hover:scale-105"
           ></div>
           <div className="relative space-y-6 p-8 sm:p-12">
-            <h3 className="text-center text-3xl font-semibold text-gray-700 dark:text-white">Organisation</h3>
+            <h3 className="text-center text-3xl font-semibold text-gray-700 dark:text-white">MemberShip</h3>
             <div>
               <div className="relative flex justify-around">
                 <div className="flex items-end">
@@ -45,18 +47,18 @@ const MemberShip = () => {
               </li>
             </ul>
             <p
-              className="mt-6 flex items-center justify-center space-x-4 text-center text-lg text-gray-600 dark:text-gray-300"
+              className="mt-6 flex whitespace-nowrap space-x-2 text-center text-lg text-gray-600 dark:text-gray-300"
             >
-              <span>Call us at</span>
-              <a href="mailto:iziaccess7@gmail.com" className="space-x-2 flex items-center text-blue-500">
-                <EnvelopeIcon className='w-6 h-6'/>
-                <span className="font-semibold">+1 000 000</span>
+              <span>Email us at:</span>
+              <a href="mailto:iziaccess7@gmail.com" className="space-x-2 flex  items-center text-blue-500">
+                <span className="font-semibold">iziaccess7@gmail.com</span>
               </a>
-              <span>or</span>
+              
             </p>
-            <a href="mailto:" className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95">
+            <div className='flex items-center justify-center text-gray-600 '>Or</div>
+            <button onClick={handleOpen} className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95">
               <span className="relative text-base font-semibold text-white dark:text-dark">Become a member</span>
-            </a>
+            </button>
           </div>
         </div>
     
