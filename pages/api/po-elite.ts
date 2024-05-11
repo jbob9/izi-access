833f1405f6e2db7202d2d5d3cded117fd468874a
jbob9 <berlenskyoliver99@gmail.com>
2024-05-11 15:04:57 -0400

    arrange po-elite apply bug 2

--- pages/api/po-elite.ts
+++ pages/api/po-elite.ts
@@ -19,7 +19,7 @@ export default async function handler(
     const form = programmeEliteFormSchema.safeParse(body);
 
     if (form.success) {
-      client.create({
+      await client.create({
         _type: "po-elite",
         ...form.data,
       });
